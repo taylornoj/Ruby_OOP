16cae218cd097d62f6f8780c1686ae858ed843d6
Taylor Noj <taylornoj@gmail.com>
2021-12-19 20:23:35 +0000

    final functionality complete for MyCar class

--- classes_and_objects_part1/01_create_class.rb
+++ classes_and_objects_part1/01_create_class.rb
@@ -1,7 +1,9 @@
 # Create a class called MyCar
+
 # When you initialize a new instance/object of the class, allow the 
 # user to define some instance variables that tell us year, color and model
-# create an instance variable set to 0 during instantiation of the object
+
+# Create an instance variable set to 0 during instantiation of the object
 # to track the current speed
 
 class MyCar
@@ -16,17 +18,32 @@ class MyCar
   # create instance methods that allow the car to speed up, break and shut off the car
 
   def speed_up(number)
+    @current_speed += number
+    puts "You push the gas pedal and accelerate #{number} kmph."
   end
 
   def break(number)
+    @current_speed -= number
+    puts "You push the break and decelerate #{number} kmph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} kmph"
   end
 
   def shut_off
+    @current_speed = 0
+    puts "Let's park this bad boy!"
   end
 
-  
+
 
 end
 
 
-
+raptor = MyCar.new(2021, 'blue', 'Ford Raptor')
+raptor.speed_up(25)
+raptor.break(15)
+raptor.current_speed
+raptor.shut_off
+raptor.current_speed
